3ba891cc2dae35c49239c8b48a890e58736723e3
Kuba <j.barszcz02@gmail.com>
2023-06-26 13:14:02 +0200

    Zadania

---
+++ Zadanie 1/Zadanie 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Podaj czas w sekundach: ");
+        int seconds = Convert.ToInt32(Console.ReadLine());
+
+        string formattedTime = FormatTime(seconds);
+        Console.WriteLine(formattedTime);
+    }
+
+    static string FormatTime(int totalSeconds)
+    {
+        int hours = totalSeconds / 3600;
+        int minutes = (totalSeconds % 3600) / 60;
+        int seconds = totalSeconds % 60;
+
+        string formattedHours = hours.ToString("D2");
+        string formattedMinutes = minutes.ToString("D2");
+        string formattedSeconds = seconds.ToString("D2");
+
+        string formattedTime = $"{formattedHours}:{formattedMinutes}:{formattedSeconds}";
+
+        return formattedTime;
+    }
+}
---
+++ Zadanie 2/Zadanie 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x1 = -2;
+        double y1 = 2;
+        double x2 = 3;
+        double y2 = 3 ;
+
+        double distance = Dystans(x1, y1, x2, y2);
+        Console.WriteLine("Długość odcinka: " + distance);
+    }
+
+    static double Dystans(double x1, double y1, double x2, double y2)
+    {
+        double deltaX = x2 - x1;
+        double deltaY = y2 - y1;
+
+        double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        return distance;
+    }
+}
